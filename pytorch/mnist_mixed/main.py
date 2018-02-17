6ec690f824feeab9097d05e622b4bb1e3eeb6c98
adityarajagopal <ar4414@ic.ac.uk>
2018-02-17 23:40:05 +0000

    cuda compatible

--- pytorch/mnist_mixed/main.py
+++ pytorch/mnist_mixed/main.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import argparse
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,6 +26,10 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
+parser.add_argument('--sf', type=float, default=128.0, metavar='N',
+                    help='scaling factor initialisation')
+parser.add_argument('--fp16', type=bool, default=False, metavar='N',
+                    help='run mixed precision training or not')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -37,12 +42,14 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.Compose([
+                       transforms.Resize((32,32)),
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                       transforms.Resize((32,32)),
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
@@ -52,48 +59,84 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        
+        self.convNet = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=(5,5)), 
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=(2,2), stride=2), 
+            nn.Conv2d(6, 16, kernel_size=(5,5)),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=(2,2), stride=2), 
+            nn.Conv2d(16, 120, kernel_size=(5,5)),
+            nn.ReLU() 
+        )
+        
+        self.fullyConn = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10),
+            nn.LogSoftmax()
+        )
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        x = self.convNet(x)
+        x = x.view(-1,120)
+        x = self.fullyConn(x)
+        return x
 
 model = Net()
 if args.cuda:
-    model.cuda()
+    if args.fp16 : 
+        model.cuda().half()
+    else : 
+        model.cuda()
 
-masterCopy = [param.clone().type(torch.FloatTensor).detach() for param in model.parameters()]
-for param in masterCopy : 
-    param.requires_grad = True
-# optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-optimizer = optim.SGD(masterCopy, lr=args.lr, momentum=args.momentum)
-
-model = model.half()
+optimizer = None 
+masterCopy = None
+if args.fp16 : 
+    masterCopy = [param.clone().type(torch.cuda.FloatTensor).detach() for param in model.parameters()]
+    for param in masterCopy : 
+        param.requires_grad = True
+    optimizer = optim.SGD(masterCopy, lr=args.lr, momentum=args.momentum)
+else : 
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data = data.half()
+            if args.fp16 : 
+                data, target = data.cuda().half(), target.cuda()
+            else : 
+                data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
         
-        # optimizer.zero_grad()
-        model.zero_grad()
-        
         output = model.forward(data)
-        loss = F.nll_loss(data, target)
+        optimizer.zero_grad()
+        loss = F.nll_loss(output, target)
+        if args.fp16 :
+            loss = loss * args.sf
         loss.backward()
+        
+        if args.fp16 :   
+            params = list(model.parameters())
+            applyGrad = True
+            for param in params : 
+                if np.isnan(np.sum(param.cpu().numpy())) or np.isinf(np.sum(param.cpu().nump())): 
+                    print ("Nan/Inf found") 
+                    args.sf /= 2.0
+                    applyGrad = False
+                    break 
+                args.sf *= 2.0
+            if applyGrad == True : 
+                for i in range(len(params)) :
+                    masterCopy[i].data -= (args.lr * (params[i].grad.data.type(torch.cuda.FloatTensor)/args.sf))
+        
         optimizer.step()
+        
+        # for i in range(len(params)) : 
+        #     params[i].data.copy_(masterCopy[i].data)
+        
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
